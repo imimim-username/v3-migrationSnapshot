559ee1a0d18be6a1134282c7347760c61ec9e301
imimim <imimim.email@gmail.com>
2026-02-03 11:03:21 -0500

    aleth pivot tables

---
+++ sum.py
@@ -0,0 +1,87 @@
+#sum up the balances per user per chain per alchemist
+#also normalize all balances to be 18 decimals
+
+import pandas as pd
+
+def alEthUnderlyingBalances(inputDf):
+    # returns a dataframe of aleth DFs that includes the underlying balances
+
+    inputDf["shares"] = df["shares"].map(int)
+    inputDf["underlyingTokensPerShare"] = df["underlyingTokensPerShare"].map(int)
+
+    
+    inputDf['underlyingValue'] = inputDf['shares'] * inputDf['underlyingTokensPerShare'] / 1e18
+
+    """
+    pivotDf = (
+    inputDf
+        .groupby("address", as_index=False)["underlyingValue"]
+        .sum()
+    )
+    return pivotDf
+    """
+
+    return inputDf
+
+def alEthFilterAndPivot(inputDf):
+    # filters out low underlyingValue balances from aleth DFs and then creates a pivot table that sums underlying value by address
+
+    lowValue = 100000000000000
+    inputDf = inputDf[inputDf['underlyingValue'] > lowValue]
+    
+    pivotDf = (
+        inputDf
+            .groupby("address", as_index=False)["underlyingValue"]
+            .sum()
+    )
+    
+    return pivotDf
+
+
+originFiles = {
+    'mainnet': 'MainnetBalances-long_script.csv',
+    'optimism': 'OptimismBalances-long_script.csv',
+    'arbitrum': 'ArbitrumBalances-long_script.csv',
+}
+
+originDataFrames = {
+    'mainnet': pd.read_csv(originFiles['mainnet']),
+    'optimism': pd.read_csv(originFiles['optimism']),
+    'arbitrum': pd.read_csv(originFiles['arbitrum']),
+}
+
+
+sixDecimals = {
+    'mainnet': ['yvUSDC', 'yvUSDT', 'aUSDC', 'aUSDT'],
+    'optimism': ['aUSDC', 'aUSDT', 'ysUSDC'],
+    'arbitrum': ['aUSDC']
+}
+
+
+weirdo = 'vaUSDC'
+
+for chain, df in originDataFrames.items():
+    '''
+    print(f"Processing {chain} data...")
+    print(df.head())
+    print(df.columns)
+    print(df.shape)
+    print(df.info())
+    print(df.describe())
+    print(df.head())
+    print(df.columns)
+    print(df.shape)
+    print(df.info())
+    print(df.describe())
+    '''
+    alethDf = df[df['alchemist'] == 'alETH'].copy()
+    alusdDf = df[df['alchemist'] == 'alUSD'].copy()
+
+    alethDf = alEthUnderlyingBalances(alethDf)
+    alethPivotDf = alEthFilterAndPivot(alethDf)
+    print(alethPivotDf.head())
+
+    fileName = 'alEthValues-' + chain + '.csv'
+    print(f"Saving {fileName}...")
+    alethPivotDf.to_csv(fileName, index=False)
+    print(f"Saved {fileName}")
